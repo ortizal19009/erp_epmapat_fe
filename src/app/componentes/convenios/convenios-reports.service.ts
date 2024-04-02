fbfd6dc11a4b283c9dee0cd4931ada5799f4adea
ortizal19009 <ort19009@byui.edu>
2024-04-02 00:55:10 -0500

    update convenios de pago

--- src/app/componentes/convenios/convenios-reports.service.ts
+++ src/app/componentes/convenios/convenios-reports.service.ts
@@ -10,6 +10,7 @@ export class ConveniosReportsService {
   constructor(private header: TemplateHeaderService) {}
   impContratoConvenio(datos: any, cuotas: any) {
     console.log(datos);
+    console.log(cuotas)
     let margin = 30;
     let doc = new jsPDF('p', 'pt', 'a4');
     let meses = [
@@ -50,13 +51,13 @@ export class ConveniosReportsService {
       datos.nomcli
     } en su calidad de solicitante, las partes en forma libre y voluntaria, convienen en suscribir el presente convenio, al tenor de las clusulas siguientes: `;
     let p2 = `PRIMERA.- ANTECEDENTES:`;
-    let p3 = `1.- El deudor posee una deuda por Titulo(s) de AGUA Y ALCANTARILLADO a nombre de la ${empresa}, de referencia Nro.: ${datos.referencia}. El deudor por el momento no cuentga con los recursos económicos necesarios para cancelar los valores adeudados.`;
-    let p4 = `2.- Mediante autorización de fecha 26-01-2024, el (la) ${administradores[0].nombre} ${administradores[0].cargo} autoriza se realice el correspondiente convenio de pago en un plazo de 365 días.`;
+    let p3 = `1.- El deudor posee una deuda por Titulo(s) de AGUA Y ALCANTARILLADO a nombre de la ${empresa}, de referencia Nro.: ${datos.referencia}. El deudor por el momento no cuenta con los recursos económicos necesarios para cancelar los valores adeudados.`;
+    let p4 = `2.- Mediante autorización, el (la) ${administradores[0].nombre} ${administradores[0].cargo} autoriza se realice el correspondiente convenio de pago en un plazo de ${cuotas.length} meses.`;
     let p5 = `SEGUNDA.- OBJETO:`;
     let p6 = `El(la) deudor se compromete a cancelar a la ${empresa} el valor adeudado, que en total es de $${datos.totalconvenio}, de acuerdo a la fórmula de pago determinada en la cláusula siguiente: por título(s) pendiente(s) de pago a la ${empresa}.`;
     let p7 = `TERCERA.- FORMA DE PAGO: `;
-    let p8 = `El(la) solicitante depositará en la ventanilla de recaudación el pago de acuerdo a la tabla qaue se adjunta al presente convenio:`;
-    let p9 = `Siendo un total de ${datos.totalconvenio}, a partir de la fecha de suscripción de este convenio hasta su termicación en el lapso de 365 diás, 12 meses, más la carta marcada en el transcurso del año.`;
+    let p8 = `El(la) solicitante depositará en la ventanilla de recaudación el pago de acuerdo a la tabla que se adjunta al presente convenio:`;
+    let p9 = `Siendo un total de ${datos.totalconvenio}, a partir de la fecha de suscripción de este convenio hasta su termicación en el lapso de ${cuotas.length} meses, más la carta marcada en el transcurso del año.`;
     let p10 = `CUARTA.- SANCIÓN: `;
     let p11 = `En caso de que el(la) deudor(a) no cancel los valores pactados en la fecha establecida, se indicará y/o continuará el juicio de coactivas en su contra, sin lugar a prorroga o plazo alguno.`;
     let p12 = `Para constancia de lo estipulado en las cláusulas anteriores, las partes se afirman y ratifican en cada una de ellas, por lo que libre y voluntariamente firman el presente conveio.`;
