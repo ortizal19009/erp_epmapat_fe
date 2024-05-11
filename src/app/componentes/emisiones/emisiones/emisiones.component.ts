05115d2c884f28230a0be299ea71f46ab4639ba8
ortizal19009 <ort19009@byui.edu>
2024-05-11 11:38:56 -0500

    update

--- src/app/componentes/emisiones/emisiones/emisiones.component.ts
+++ src/app/componentes/emisiones/emisiones/emisiones.component.ts
@@ -22,6 +22,7 @@ import { AbonadosService } from 'src/app/servicios/abonados.service';
 import { FacturaService } from 'src/app/servicios/factura.service';
 import { NovedadesService } from 'src/app/servicios/novedades.service';
 import { Pliego24Service } from 'src/app/servicios/pliego24.service';
+import { RubroxfacService } from 'src/app/servicios/rubroxfac.service';
 
 @Component({
   selector: 'app-emisiones',
@@ -65,16 +66,23 @@ export class EmisionesComponent implements OnInit {
   fechaemision: Date;
   novedades: any;
   /* emision individual */
-  _lecturas: any = []
+  _lecturas: any = [];
   tarifa: any;
   rubros: any = [];
   _rubroxfac: any;
   newtotal: number;
+  lecturaestado: number = 0;
+  idfactura: number;
+  ctotal: number;
 
-  porcResidencial: number[] = [0.777, 0.78, 0.78, 0.78, 0.78, 0.778, 0.778, 0.778, 0.78, 0.78, 0.78, 0.68, 0.68, 0.678, 0.68, 0.68, 0.678, 0.678, 0.68,
-    0.68, 0.678, 0.676, 0.678, 0.678, 0.678, 0.68, 0.647, 0.65, 0.65, 0.647, 0.647, 0.65, 0.65, 0.647, 0.647, 0.65, 0.65, 0.65,
-    0.65, 0.65, 0.65, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7,
-    0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7]
+  porcResidencial: number[] = [
+    0.777, 0.78, 0.78, 0.78, 0.78, 0.778, 0.778, 0.778, 0.78, 0.78, 0.78, 0.68,
+    0.68, 0.678, 0.68, 0.68, 0.678, 0.678, 0.68, 0.68, 0.678, 0.676, 0.678,
+    0.678, 0.678, 0.68, 0.647, 0.65, 0.65, 0.647, 0.647, 0.65, 0.65, 0.647,
+    0.647, 0.65, 0.65, 0.65, 0.65, 0.65, 0.65, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7,
+    0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7,
+    0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7,
+  ];
 
   constructor(
     public fb: FormBuilder,
@@ -88,8 +96,9 @@ export class EmisionesComponent implements OnInit {
     private aboService: AbonadosService,
     private facService: FacturaService,
     private s_novedades: NovedadesService,
-    private pli24Service: Pliego24Service
-  ) { }
+    private pli24Service: Pliego24Service,
+    private rxfService: RubroxfacService
+  ) {}
 
   ngOnInit(): void {
     this.modulo.idmodulo = 4;
@@ -126,8 +135,8 @@ export class EmisionesComponent implements OnInit {
     this.f_lecturas = this.fb.group({
       lecturaanterior: 0,
       lecturaactual: 0,
-      idnovedad_novedades: ''
-    })
+      idnovedad_novedades: '',
+    });
     let date: Date = new Date();
     this.formAddEmision = this.fb.group({
       emision: '',
@@ -334,7 +343,7 @@ export class EmisionesComponent implements OnInit {
   /*=====================
   ======INDIVIDUALES=====
   =====================*/
-  emisionIndividual() { }
+  emisionIndividual() {}
   getAllEmisiones() {
     this.emiService.findAllEmisiones().subscribe({
       next: (datos: any) => {
@@ -364,29 +373,27 @@ export class EmisionesComponent implements OnInit {
       .subscribe({
         next: (datos: any) => {
           console.log(datos);
-          this._lectura = datos;
+          this._lectura = datos[0];
           this.f_lecturas.patchValue({
-            lecturaanterior: datos.lecturaanterior,
-            lecturaactual: datos.lecturaactual,
-            idnovedad_novedades: datos.idnovedad_novedades
-          })
+            lecturaanterior: datos[0].lecturaanterior,
+            lecturaactual: datos[0].lecturaactual,
+            idnovedad_novedades: datos[0].idnovedad_novedades,
+          });
         },
         error: (e) => console.error(e),
       });
-
   }
-  // generarLecturaIndividual(){
+  //Generar nueva emision individual(){
   saveEmisionIndividual() {
-    console.log(this.f_emisionIndividual.value)
-    console.log(this._lectura)
+    console.log(this.f_emisionIndividual.value);
+    console.log(this._lectura);
     let lectura: Lecturas = new Lecturas();
-    console.log(this.f_lecturas.value)
-
-
+    console.log(this.f_lecturas.value);
+    this.generaRutaxemisionIndividual();
   }
   async generaRutaxemisionIndividual() {
     if (this.cerrado === 0) {
-      let fecha: Date = new Date();
+      this.lecturaestado = 0;
       let novedad: Novedad = new Novedad();
       novedad.idnovedad = 1;
       let rutasxemision = {} as Rutasxemision;
@@ -394,25 +401,40 @@ export class EmisionesComponent implements OnInit {
       let ruta: Rutas = new Rutas();
       ruta.idruta = this.ruta.idruta;
       emision.idemision = this.idemision;
-      this.ruxemiService.getByEmisionRuta(emision.idemision, ruta.idruta).subscribe({
-        next: (datos: any) => {
-          console.log(datos)
-          rutasxemision = datos
-          this.generaLecturaIndividual(rutasxemision, novedad);
-        }, error: (e) => console.error(e)
-      })
-      try {
-        /*       const nuevaRutaxemision = await this.ruxemiService.updateRutaxemision(rutasxemision.idrutaxemision,
-          rutasxemision
-        ); */
-      } catch (error) {
-        console.error(`Al guardar Rutaxemision `, error);
-      }
+      this.ruxemiService
+        .getByEmisionRuta(emision.idemision, ruta.idruta)
+        .subscribe({
+          next: (datos: any) => {
+            rutasxemision = datos;
+            this.generaLecturaIndividual(rutasxemision, novedad);
+          },
+          error: (e) => console.error(e),
+        });
+    }
+    if (this.cerrado === 1) {
+      this.lecturaestado = 1;
+      let novedad: Novedad = new Novedad();
+      novedad.idnovedad = this.f_lecturas.value.idnovedad_novedades;
+      let rutasxemision = {} as Rutasxemision;
+      let emision: Emisiones = new Emisiones();
+      let ruta: Rutas = new Rutas();
+      ruta.idruta = this.ruta.idruta;
+      emision.idemision = this.idemision;
+      this.ruxemiService
+        .getByEmisionRuta(emision.idemision, ruta.idruta)
+        .subscribe({
+          next: (datos: any) => {
+            rutasxemision = datos;
+            this.generaLecturaIndividual(rutasxemision, novedad);
+          },
+          error: (e) => console.error(e),
+        });
     }
   }
   //Genera las lecturas de los abonados de la nueva Rutaxemision
   async generaLecturaIndividual(nuevarutaxemi: any, novedad: any) {
     try {
+      let dateEmision: Date = new Date();
       // for (let k = 0; k < 2; k++) {
       //Primero crea la Factura (Planilla) para cada Abonado para luego ponerla en las Lecturas
       let planilla = {} as Planilla;
@@ -430,20 +452,21 @@ export class EmisionesComponent implements OnInit {
       planilla.valorbase = 0;
       planilla.usucrea = this.authService.idusuario;
       planilla.estado = 1;
-      planilla.feccrea = new Date(this.fechaemision);
+      planilla.feccrea = dateEmision;
       //let nuevoIdfactura = k; Solo para pruebas, para que no genere las facturas
       let nuevoIdfactura: number = 0;
       try {
         //Crea la planilla con el metodo que devuelve el idfactura generado
         nuevoIdfactura = await this.facService.saveFacturaAsyncId(planilla);
+        this.idfactura = nuevoIdfactura;
       } catch (error) {
         console.error(`Al guardar la Factura (planilla)`, error);
       }
       //Ahora si crea la Lectura para cada Abonado
       let lectura = {} as Lectura;
-      lectura.estado = 0;
-      lectura.fechaemision = this.fechaemision;
-      try {
+      lectura.estado = this.lecturaestado;
+      lectura.fechaemision = dateEmision;
+      /*       try {
         let lecturaanterior = await this.s_lecturas.getUltimaLecturaAsync(
           this.abonado.idabonado
         );
@@ -453,8 +476,9 @@ export class EmisionesComponent implements OnInit {
         lectura.lecturaanterior = lecturaanterior;
       } catch (error) {
         console.error(`Al buscar la Última lectura`, error);
-      }
-      lectura.lecturaactual = this.f_lecturas.value.lecturaanterior;
+      } */
+      lectura.lecturaanterior = this.f_lecturas.value.lecturaanterior;
+      lectura.lecturaactual = this.f_lecturas.value.lecturaactual;
       lectura.lecturadigitada = this.f_lecturas.value.lecturaactual;
       lectura.mesesmulta = 0;
       lectura.idnovedad_novedades = novedad;
@@ -466,7 +490,8 @@ export class EmisionesComponent implements OnInit {
       lectura.total1 = 0;
       lectura.idfactura = nuevoIdfactura;
       try {
-        await this.s_lecturas.saveLecturaAsync(lectura);
+        let newLectura = await this.s_lecturas.saveLecturaAsync(lectura);
+        await this.planilla(newLectura);
       } catch (error) {
         console.error(`Al guardar La lectura`, error);
       }
@@ -475,38 +500,78 @@ export class EmisionesComponent implements OnInit {
     }
   }
 
-  planilla() {
+  async planilla(lectura: Lecturas) {
+    console.log('LECTURA CREADA', lectura);
     //this.swcalcular = true;
-    let categoria = this._lecturas[0].idabonado_abonados.idcategoria_categorias.idcategoria;
-    let consumo = this._lecturas[0].lecturaactual - this._lecturas[0].lecturaanterior;
-    let adultomayor = this._lecturas[0].idabonado_abonados.adultomayor;
-    let noAlcantarillado = this._lecturas[0].idabonado_abonados.swalcantarillado;
-    if (adultomayor) if (categoria == 9 && consumo > 34) categoria = 1;
-    else if (categoria == 9 && consumo > 10) categoria = 1;
+    let categoria =
+      lectura.idabonado_abonados.idcategoria_categorias.idcategoria;
+    let consumo = lectura.lecturaactual - lectura.lecturaanterior;
+    let adultomayor = lectura.idabonado_abonados.adultomayor;
+    let noAlcantarillado = lectura.idabonado_abonados.swalcantarillado;
+    if (adultomayor)
+      if (categoria == 9 && consumo > 34) categoria = 1;
+      else if (categoria == 9 && consumo > 10) categoria = 1;
     if (categoria == 9 && consumo > 34) categoria = 1;
     if (categoria == 1 && consumo > 70) categoria = 2;
-    let municipio = this._lecturas[0].idabonado_abonados.municipio;
+    let municipio = lectura.idabonado_abonados.municipio;
     let num1: number;
-    let swcate9: boolean;     //No hay Tarifas para Categoria 9 es el 50% de la 1
-    if (categoria == 9) { categoria = 1; swcate9 = true }
+    let swcate9: boolean; //No hay Tarifas para Categoria 9 es el 50% de la 1
+    if (categoria == 9) {
+      categoria = 1;
+      swcate9 = true;
+    }
     let swmunicipio: boolean; //Instituciones del Municipio 50% de la Tarifa Oficial
-    if (categoria == 4 && municipio) { swmunicipio = true }
+    if (categoria == 4 && municipio) {
+      swmunicipio = true;
+    }
+    console.log('categoria y consumo ', categoria, '----', consumo);
     // Obtiene la tarifa del nuevo Pliego
     this.pli24Service.getBloque(categoria, consumo).subscribe({
-      next: resp => {
+      next: (resp) => {
+        console.log('obener tarifa', resp);
         if (!resp) {
           //No hay Tarifa para esta Categoría y Consumo
-        }
-        else {
+        } else {
           this.tarifa = resp;
 
-          if (categoria == 1) { num1 = Math.round((this.tarifa[0].idcategoria.fijoagua - 0.1) * this.porcResidencial[consumo] * 100) / 100; }
-          else { num1 = Math.round((this.tarifa[0].idcategoria.fijoagua - 0.1) * this.tarifa[0].porc * 100) / 100; }
+          if (categoria == 1) {
+            num1 =
+              Math.round(
+                (this.tarifa[0].idcategoria.fijoagua - 0.1) *
+                  this.porcResidencial[consumo] *
+                  100
+              ) / 100;
+          } else {
+            num1 =
+              Math.round(
+                (this.tarifa[0].idcategoria.fijoagua - 0.1) *
+                  this.tarifa[0].porc *
+                  100
+              ) / 100;
+          }
 
-          let num2 = Math.round((this.tarifa[0].idcategoria.fijosanea - 0.5) * this.tarifa[0].porc * 100) / 100;
-          let num3 = Math.round((consumo * this.tarifa[0].agua) * this.tarifa[0].porc * 100) / 100;
-          let num4 = Math.round((consumo * this.tarifa[0].saneamiento / 2) * this.tarifa[0].porc * 100) / 100;
-          let num5 = Math.round((consumo * this.tarifa[0].saneamiento / 2) * this.tarifa[0].porc * 100) / 100;
+          let num2 =
+            Math.round(
+              (this.tarifa[0].idcategoria.fijosanea - 0.5) *
+                this.tarifa[0].porc *
+                100
+            ) / 100;
+          let num3 =
+            Math.round(
+              consumo * this.tarifa[0].agua * this.tarifa[0].porc * 100
+            ) / 100;
+          let num4 =
+            Math.round(
+              ((consumo * this.tarifa[0].saneamiento) / 2) *
+                this.tarifa[0].porc *
+                100
+            ) / 100;
+          let num5 =
+            Math.round(
+              ((consumo * this.tarifa[0].saneamiento) / 2) *
+                this.tarifa[0].porc *
+                100
+            ) / 100;
           let num7 = Math.round(0.5 * this.tarifa[0].porc * 100) / 100;
           let suma: number = 0;
           if (noAlcantarillado === true) {
@@ -515,43 +580,79 @@ export class EmisionesComponent implements OnInit {
             num7 = 0;
             num5 = 0;
           }
-          suma = Math.round((num1 + num2 + num3 + num4 + num5 + 0.1 + num7) * 100) / 100;
+          suma =
+            Math.round((num1 + num2 + num3 + num4 + num5 + 0.1 + num7) * 100) /
+            100;
           //Categoria 9 no tiene tarifario es el 50% de la Residencial. Abonados del Municipio también 50%
-          if (swcate9 || swmunicipio) suma = Math.round(suma / 2 * 100) / 100;
+          if (swcate9 || swmunicipio) suma = Math.round((suma / 2) * 100) / 100;
           this.newtotal = suma;
           let r: any = {};
           this.rubros = [];
           if (swcate9 || swmunicipio) {
-            let rub1002: number; let rub1003: number
+            let rub1002: number;
+            let rub1003: number;
             //Alcantarillado / 2
-            if (num2 + num4 + num7 > 0) rub1002 = Math.round((num2 + num4 + num7) / 2 * 100) / 100;
+            if (num2 + num4 + num7 > 0)
+              rub1002 = Math.round(((num2 + num4 + num7) / 2) * 100) / 100;
             else rub1002 = 0;
             //Saneamiento / 2
-            if (num5 > 0) rub1003 = Math.round(num5 / 2 * 100) / 100;
-            else rub1003 = 0
+            if (num5 > 0) rub1003 = Math.round((num5 / 2) * 100) / 100;
+            else rub1003 = 0;
             //Agua portable por diferencia con la suma
             let rub1001 = suma - rub1002 - rub1003 - 0.1;
-            r = { idrubroxfac: this._rubroxfac[0].idrubroxfac, descripcion: this._rubroxfac[0].idrubro_rubros.descripcion, valorunitario: rub1001 };
+            r = {
+              c: this._rubroxfac[0].idrubroxfac,
+              descripcion: this._rubroxfac[0].idrubro_rubros.descripcion,
+              valorunitario: rub1001,
+            };
             this.rubros.push(r);
-            r = { idrubroxfac: this._rubroxfac[1].idrubroxfac, descripcion: this._rubroxfac[1].idrubro_rubros.descripcion, valorunitario: rub1002 };
+            r = {
+              idrubroxfac: this._rubroxfac[1].idrubroxfac,
+              descripcion: this._rubroxfac[1].idrubro_rubros.descripcion,
+              valorunitario: rub1002,
+            };
             this.rubros.push(r);
-            r = { idrubroxfac: this._rubroxfac[2].idrubroxfac, descripcion: this._rubroxfac[2].idrubro_rubros.descripcion, valorunitario: rub1003 };
+            r = {
+              idrubroxfac: this._rubroxfac[2].idrubroxfac,
+              descripcion: this._rubroxfac[2].idrubro_rubros.descripcion,
+              valorunitario: rub1003,
+            };
             this.rubros.push(r);
-            r = { idrubroxfac: this._rubroxfac[3].idrubroxfac, descripcion: this._rubroxfac[3].idrubro_rubros.descripcion, valorunitario: 0.1 };
+            r = {
+              idrubroxfac: this._rubroxfac[3].idrubroxfac,
+              descripcion: this._rubroxfac[3].idrubro_rubros.descripcion,
+              valorunitario: 0.1,
+            };
             this.rubros.push(r);
           } else {
-            r = { idrubroxfac: this._rubroxfac[0].idrubroxfac, descripcion: this._rubroxfac[0].idrubro_rubros.descripcion, valorunitario: num1 + num3 };
+            r = {
+              idrubroxfac: this._rubroxfac[0].idrubroxfac,
+              descripcion: this._rubroxfac[0].idrubro_rubros.descripcion,
+              valorunitario: num1 + num3,
+            };
             this.rubros.push(r);
-            r = { idrubroxfac: this._rubroxfac[1].idrubroxfac, descripcion: this._rubroxfac[1].idrubro_rubros.descripcion, valorunitario: num2 + num4 + num7 };
+            r = {
+              idrubroxfac: this._rubroxfac[1].idrubroxfac,
+              descripcion: this._rubroxfac[1].idrubro_rubros.descripcion,
+              valorunitario: num2 + num4 + num7,
+            };
             this.rubros.push(r);
-            r = { idrubroxfac: this._rubroxfac[2].idrubroxfac, descripcion: this._rubroxfac[2].idrubro_rubros.descripcion, valorunitario: num5 };
+            r = {
+              idrubroxfac: this._rubroxfac[2].idrubroxfac,
+              descripcion: this._rubroxfac[2].idrubro_rubros.descripcion,
+              valorunitario: num5,
+            };
             this.rubros.push(r);
-            r = { idrubroxfac: this._rubroxfac[3].idrubroxfac, descripcion: this._rubroxfac[3].idrubro_rubros.descripcion, valorunitario: 0.1 };
+            r = {
+              idrubroxfac: this._rubroxfac[3].idrubroxfac,
+              descripcion: this._rubroxfac[3].idrubro_rubros.descripcion,
+              valorunitario: 0.1,
+            };
             this.rubros.push(r);
-          };
+          }
         }
       },
-      error: err => console.error(err.error)
+      error: (err) => console.error(err.error),
     });
   }
   getAllNovedades() {
@@ -571,6 +672,27 @@ export class EmisionesComponent implements OnInit {
         ? false
         : o1.idnovedad == o2.idnovedad;
     }
+  }
+
+  rubroxfac() {
+    this.rxfService.getByIdfactura(this.idfactura).subscribe({
+      next: (datos) => {
+        this._rubroxfac = datos;
+        this.csubtotal();
+      },
+      error: (err) => console.error(err.error),
+    });
+  }
+
+  csubtotal() {
+    let suma: number = 0;
+    let i = 0;
+    this._rubroxfac.forEach(() => {
+      suma += this._rubroxfac[i].cantidad * this._rubroxfac[i].valorunitario;
+      i++;
+    });
+    this.ctotal = Math.round(suma * 100) / 100;
+    //this.finbusca = true
   }
 
   /* ===================================== */
