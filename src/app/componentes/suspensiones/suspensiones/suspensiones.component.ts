4991a9c74f0e5fd3873755c9417b1ee4da60bff7
ortizln <ortizln9@gmail.com>
2024-06-17 16:36:13 -0500

    update

--- src/app/componentes/suspensiones/suspensiones/suspensiones.component.ts
+++ src/app/componentes/suspensiones/suspensiones/suspensiones.component.ts
@@ -140,20 +140,31 @@ export class SuspensionesComponent implements OnInit {
 
     this._ruta = e;
     this.s_lecturas.findDeudoresByRuta(this._ruta.idruta).subscribe({
-      next: async (datos: any) => {
-        console.log(datos);
-        await datos.forEach((item: any) => {
+      next: async (lecturas: any) => {
+        await lecturas.forEach((item: any) => {
+          let newPreFactura: any = [];
           this.s_rubroxfac
             .getByIdfacturaAsync(item.idfactura)
             .then((i: any) => {
-              let newPreFactura: any = {};
               if (i.length > 0) {
                 let doc = new jsPDF('p', 'pt', 'a4');
+                //newPreFactura.idfactura = i.idfactura;
+                //newDatos.push(newPreFactura);
+                let findLectura = newPreFactura.find(
+                  (lectura: { idlectura: number }) =>
+                    lectura.idlectura === item.idlectura
+                );
+                if (findLectura === undefined) {
+                  newPreFactura.push({ item, i });
+                  console.log(newPreFactura);
+                } else {
+                  //newPreFactura.findLectura[]
+                }
 
-                newPreFactura.idfactura = i.idfactura;
-                newDatos.push(newPreFactura);
+                console.log(findLectura);
                 console.log(i);
               }
+              console.log(newPreFactura);
             })
             .then(async () => {
               console.log('HOLA MUNDO SEGUNDO BLOQUE');
