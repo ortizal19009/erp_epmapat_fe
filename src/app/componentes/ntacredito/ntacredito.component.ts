5b55785b4b80bd875e20ab4e81a2dab642b5dcdc
ortizln <ortizln9@gmail.com>
2025-04-09 09:56:28 -0500

    update notacredito

--- src/app/componentes/ntacredito/ntacredito.component.ts
+++ src/app/componentes/ntacredito/ntacredito.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ColoresService } from 'src/app/compartida/colores.service';
+import { NtacreditoService } from 'src/app/servicios/ntacredito.service';
 
 @Component({
   selector: 'app-ntacredito',
@@ -12,13 +13,27 @@ export class NtacreditoComponent implements OnInit {
   f_bntacredito: FormGroup;
   filterTerm: string;
   _ntacreditos: any;
-  constructor(private router: Router, private coloresService: ColoresService) {}
+  /* variables para hacer la paginación  */
+  page: number = 0;
+  size: number = 10;
+  _cuentasPageables?: any;
+  totalPages: number = 0; // Total de páginas
+  pages: number[] = []; // Lista de números de página
+  maxPagesToShow: number = 5;
+  constructor(private router: Router, private coloresService: ColoresService, private s_ntacredito: NtacreditoService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     sessionStorage.setItem('ventana', '/ntacredito');
     let coloresJSON = sessionStorage.getItem('/ntacredito');
     if (coloresJSON) this.colocaColor(JSON.parse(coloresJSON));
     else this.buscaColor();
+    this.f_bntacredito = this.fb.group({
+      selecTipoBusqueda: 1,
+      buscarAbonado: ''
+
+    })
+    this.getAllNotasCreditos(this.page, this.size)
+
   }
   async buscaColor() {
     try {
@@ -39,6 +54,48 @@ export class NtacreditoComponent implements OnInit {
     const detalle = document.querySelector('.detalle');
     if (detalle) detalle.classList.add('nuevoBG2');
   }
-  onSubmit() {}
-  detallesnotasnc(notacredito: any) {}
+  onSubmit() { }
+  detallesnotasnc(notacredito: any) { }
+  getAllNotasCreditos(page: number, size: number) {
+    this.s_ntacredito.getAllPageable(page, size).subscribe({
+      next: (datos: any) => {
+        this._ntacreditos = datos.content;
+      },
+      error: (e: any) => console.error(e)
+    })
+  }
+  /* Inicio de configuracion de paginacion */
+  onPreviousPage(): void {
+    if (this.page > 0) {
+      //this.getByPagesCuentas(this.page - 1, this.size);
+    }
+  }
+  onNextPage(): void {
+    if (this.page <= this.totalPages - 1) {
+      this.getAllNotasCreditos(this.page + 1, this.size);
+    }
+  }
+  onGoToPage(page: number): void {
+    if (page >= 0 && page < this.totalPages) {
+      this.getAllNotasCreditos(page, this.size);
+    }
+  }
+
+  updatePages(): void {
+    const halfRange = Math.floor(this.maxPagesToShow / 2);
+    let startPage = Math.max(this.page - halfRange, 0);
+    let endPage = Math.min(this.page + halfRange, this.totalPages - 1);
+    // Ajusta el rango si estás al principio o al final
+    if (this.page <= halfRange) {
+      endPage = Math.min(this.maxPagesToShow - 1, this.totalPages - 1);
+    } else if (this.page + halfRange >= this.totalPages) {
+      startPage = Math.max(this.totalPages - this.maxPagesToShow, 0);
+    }
+    // Genera los números de las páginas visibles
+    this.pages = Array.from(
+      { length: endPage - startPage + 1 },
+      (_, i) => startPage + i
+    );
+  }
+  /* Fin de configuracion de paginacion */
 }
