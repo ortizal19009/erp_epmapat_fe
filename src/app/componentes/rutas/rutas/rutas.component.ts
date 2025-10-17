d081e2aedac4b337667b2d854b2c6b67451037b8
ortizal <alexis.ortiz81@outlook.com>
2025-10-17 14:42:39 -0500

    Update

--- src/app/componentes/rutas/rutas/rutas.component.ts
+++ src/app/componentes/rutas/rutas/rutas.component.ts
@@ -6,101 +6,109 @@ import { Rutas } from 'src/app/modelos/rutas.model';
 import { RutasService } from 'src/app/servicios/rutas.service';
 
 @Component({
-   selector: 'app-rutas',
-   templateUrl: './rutas.component.html',
-   styleUrls: ['./rutas.component.css']
+  selector: 'app-rutas',
+  templateUrl: './rutas.component.html',
+  styleUrls: ['./rutas.component.css'],
 })
-
 export class RutasComponent implements OnInit {
+  filtro: string;
+  _rutas: any;
+  _deudasRuta: any;
 
+  constructor(
+    private rutService: RutasService,
+    private router: Router,
+    private coloresService: ColoresService,
+    public authService: AutorizaService
+  ) {}
 
-   filtro: string;
-   _rutas: any;
+  ngOnInit(): void {
+    sessionStorage.setItem('ventana', '/rutas');
+    let coloresJSON = sessionStorage.getItem('/rutas');
+    if (coloresJSON) this.colocaColor(JSON.parse(coloresJSON));
+    else this.buscaColor();
 
-   constructor(private rutService: RutasService, private router: Router, private coloresService: ColoresService,
-      public authService: AutorizaService) { }
+    this.listaRutas();
+  }
 
-   ngOnInit(): void {
-      this.authService.valsession();
+  // @HostListener('window:beforeunload', ['$event'])
+  // unloadHandler(event: Event): void {
+  //    alert('Alerta')
+  //    // Puedes agregar lógica aquí para determinar si permitir o no la recarga
+  //    // Por ejemplo, devolver un mensaje personalizado
+  //    // Este es el nuevo método para prevenir la acción predeterminada
+  //    event.preventDefault();
+  // }
 
-      sessionStorage.setItem('ventana', '/rutas');
-      let coloresJSON = sessionStorage.getItem('/rutas');
-      if (coloresJSON) this.colocaColor(JSON.parse(coloresJSON));
-      else this.buscaColor();
+  // onNavigationEnd() {
+  //    this.router.navigate(['/app-rutas']);
+  // }
 
-      this.listaRutas();
-   }
+  async buscaColor() {
+    try {
+      const datos = await this.coloresService.setcolor(1, 'rutas');
+      const coloresJSON = JSON.stringify(datos);
+      sessionStorage.setItem('/rutas', coloresJSON);
+      this.colocaColor(datos);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-   // @HostListener('window:beforeunload', ['$event'])
-   // unloadHandler(event: Event): void {
-   //    alert('Alerta')
-   //    // Puedes agregar lógica aquí para determinar si permitir o no la recarga
-   //    // Por ejemplo, devolver un mensaje personalizado
-   //    // Este es el nuevo método para prevenir la acción predeterminada
-   //    event.preventDefault();
-   // }
+  colocaColor(colores: any) {
+    document.documentElement.style.setProperty('--bgcolor1', colores[0]);
+    const cabecera = document.querySelector('.cabecera');
+    if (cabecera) cabecera.classList.add('nuevoBG1');
+    document.documentElement.style.setProperty('--bgcolor2', colores[1]);
+    const detalle = document.querySelector('.detalle');
+    if (detalle) detalle.classList.add('nuevoBG2');
+  }
 
-   // onNavigationEnd() {
-   //    this.router.navigate(['/app-rutas']);
-   // }
+  public listaRutas() {
+    this.rutService.getListaRutas().subscribe({
+      next: (rutas) => (this._rutas = rutas),
+      error: (err) => console.error(err.error),
+    });
+  }
 
-   async buscaColor() {
-      try {
-         const datos = await this.coloresService.setcolor(1, 'rutas');
-         const coloresJSON = JSON.stringify(datos);
-         sessionStorage.setItem('/rutas', coloresJSON);
-         this.colocaColor(datos);
-      } catch (error) {
-         console.error(error);
-      }
-   }
+  onCellClick(event: any, ruta: Rutas) {
+    const tagName = event.target.tagName;
+    if (tagName === 'TD') {
+      //  sessionStorage.setItem('codparToAuxgasto', presupue.codpar.toString());
+      //  sessionStorage.setItem('nomparToAuxiliar', presupue.nompar.toString());
+      //  sessionStorage.setItem('iniciaToAuxiliar', presupue.inicia.toString());
+      //  this.router.navigate(['aux-gasto']);
+    }
+  }
 
-   colocaColor(colores: any) {
-      document.documentElement.style.setProperty('--bgcolor1', colores[0]);
-      const cabecera = document.querySelector('.cabecera');
-      if (cabecera) cabecera.classList.add('nuevoBG1')
-      document.documentElement.style.setProperty('--bgcolor2', colores[1]);
-      const detalle = document.querySelector('.detalle');
-      if (detalle) detalle.classList.add('nuevoBG2');
-   }
+  eliminarRuta(idruta: number) {
+    this.rutService.deleteRuta(idruta).subscribe({
+      next: (datos) => this.listaRutas(),
+      error: (err) => console.error(err.error),
+    });
+  }
 
-   public listaRutas() {
-      this.rutService.getListaRutas().subscribe({
-         next: rutas => this._rutas = rutas,
-         error: err => console.error(err.error)
-      })
-   }
+  modificarRuta(ruta: Rutas) {
+    localStorage.setItem('idruta', ruta.idruta.toString());
+    this.router.navigate(['modificar-rutas']);
+  }
 
-   onCellClick(event: any, ruta: Rutas) {
-      const tagName = event.target.tagName;
-      if (tagName === 'TD') {
-         //  sessionStorage.setItem('codparToAuxgasto', presupue.codpar.toString());
-         //  sessionStorage.setItem('nomparToAuxiliar', presupue.nompar.toString());
-         //  sessionStorage.setItem('iniciaToAuxiliar', presupue.inicia.toString());
-         //  this.router.navigate(['aux-gasto']);
-      }
-   }
+  mostrarDatos(ruta: Rutas) {
+    let cod = ruta.codigo;
+    console.log(cod[0]);
+  }
 
-   eliminarRuta(idruta: number) {
-      this.rutService.deleteRuta(idruta).subscribe({
-         next: datos => this.listaRutas(),
-         error: err => console.error(err.error)
-      });
-   }
-
-   modificarRuta(ruta: Rutas) {
-      localStorage.setItem("idruta", ruta.idruta.toString());
-      this.router.navigate(['modificar-rutas']);
-   }
-
-   mostrarDatos(ruta: Rutas) {
-      let cod = ruta.codigo;
-      console.log(cod[0]);
-   }
-
-   public info(idruta: number) {
-      sessionStorage.setItem('idrutaToInfo', idruta.toString());
-      this.router.navigate(['info-ruta']);
-   }
-
+  public info(idruta: number) {
+    sessionStorage.setItem('idrutaToInfo', idruta.toString());
+    this.router.navigate(['info-ruta']);
+  }
+  getDeudasOfRuta(idruta: number) {
+    this._deudasRuta = [];
+    this.rutService.getDeudaOfCuentasByIdrutas(idruta).subscribe({
+      next: (data: any) => {
+      console.log(data)
+        this._deudasRuta = data;
+      },
+    });
+  }
 }
--- src/app/servicios/rutas.service.ts
+++ src/app/servicios/rutas.service.ts
@@ -8,44 +8,50 @@ const apiUrl = environment.API_URL;
 const baseUrl = `${apiUrl}/rutas`;
 
 @Injectable({
-   providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class RutasService {
+  constructor(private http: HttpClient) {}
 
-   constructor(private http: HttpClient) { }
+  getListaRutas(): Observable<Rutas[]> {
+    return this.http.get<Rutas[]>(`${baseUrl}`);
+  }
 
-   getListaRutas(): Observable<Rutas[]> {
-      return this.http.get<Rutas[]>(`${baseUrl}`);
-   }
+  saveRuta(ruta: Rutas): Observable<Object> {
+    return this.http.post(`${baseUrl}`, ruta);
+  }
 
-   saveRuta(ruta: Rutas): Observable<Object> {
-      return this.http.post(`${baseUrl}`, ruta);
-   }
+  deleteRuta(idruta: number): Observable<Object> {
+    return this.http.delete(`${baseUrl}/${idruta}`);
+  }
 
-   deleteRuta(idruta: number): Observable<Object> {
-      return this.http.delete(`${baseUrl}/${idruta}`);
-   }
+  updateRuta(ruta: Rutas) {
+    return this.http.put(`${baseUrl}/${ruta.idruta}`, ruta);
+  }
 
-   updateRuta(ruta: Rutas) {
-      return this.http.put(`${baseUrl}/${ruta.idruta}`, ruta);
-   }
+  getByIdruta(idruta: number) {
+    return this.http.get<Rutas>(`${baseUrl}/${idruta}`);
+  }
 
-   getByIdruta(idruta: number) {
-      return this.http.get<Rutas>(`${baseUrl}/${idruta}`);
-   }
+  getRutasByQuery(dato: String) {
+    return this.http.get<Rutas>(`${baseUrl}?rutaConsulta=${dato}`);
+  }
 
-   getRutasByQuery(dato: String) {
-      return this.http.get<Rutas>(`${baseUrl}?rutaConsulta=${dato}`);
-   }
+  //Valida el Código
+  valCodigo(codigo: string): Observable<boolean> {
+    return this.http.get<boolean>(`${baseUrl}/valCodigo?codigo=${codigo}`);
+  }
 
-   //Valida el Código
-   valCodigo(codigo: string): Observable<boolean> {
-      return this.http.get<boolean>(`${baseUrl}/valCodigo?codigo=${codigo}`);
-   }
-
-   getNcuentasByRutas(){
-      return this.http.get(`${baseUrl}/cuentasByRuta`);
-   }
-
+  getNcuentasByRutas() {
+    return this.http.get(`${baseUrl}/cuentasByRuta`);
+  }
+  getDeudaOfCuentasByIdrutas(idruta: number) {
+    return this.http.get(`${baseUrl}/deudas_ruta_cuentas?idruta=${idruta}`);
+  }
+  getDeudasOfAllCuentas() {
+    return this.http.get(`${baseUrl}/deudas_cuentas`);
+  }
+  getDeudasOfRutas() {
+    return this.http.get(`${baseUrl}/deudas_rutas`);
+  }
 }
