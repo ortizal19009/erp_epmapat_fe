9784c73c6eca77f17ac665de4525f270851d43b6
ortizln <ortizln9@gmail.com>
2026-03-12 15:13:40 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   //API_URL: 'http://192.168.0.71:9090'
-  API_URL: 'http://192.168.0.88:8888/erp_epmapat-v0.1'
+  //API_URL: 'http://192.168.0.88:8888/erp_epmapat-v0.1'
   //API_URL: 'http://192.168.100.16:9080/erp_epmapat-v0.1'
+  API_URL: 'http://192.168.0.46:8080'
 };
